12f26b5d57475cfedd735731585eafd8ff75ff57
hono63 <hono6363@yahoo.co.jp>
2023-01-06 00:01:03 +0900

    reactive

--- HelloVue/src/App.vue
+++ HelloVue/src/App.vue
@@ -2,8 +2,20 @@
 import {ref} from "vue"
 
 const name = ref("Ino Junsuke")
+const now = new Date();
+const nowStr = now.toLocaleTimeString();
+const timeRef = ref(nowStr);
+
+function updateTime(): void {
+  const newTime = new Date();
+  const newTimeStr = newTime.toLocaleTimeString();
+  timeRef.value = newTimeStr;
+}
+
+setInterval(updateTime, 1000);
 </script>
 
 <template>
   <h1>ちわーっす {{ name }} くん</h1>
+  いま {{ timeRef }} だよ
 </template>
