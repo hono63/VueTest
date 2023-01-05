8ca6767eea07b7d8dac938358f6690e4bda4cc2c
hono63 <hono6363@yahoo.co.jp>
2023-01-06 00:19:04 +0900

    reactive data

--- HelloVue/src/App.vue
+++ HelloVue/src/App.vue
@@ -1,29 +1,32 @@
 <script setup lang="ts">
-import {ref, computed} from "vue"
+import {ref, computed, reactive} from "vue"
 
 const name = ref("Ino Junsuke")
-const now = new Date();
-const nowStr = now.toLocaleTimeString();
-const timeRef = ref(nowStr);
 
-function updateTime(): void {
-  const newTime = new Date();
-  const newTimeStr = newTime.toLocaleTimeString();
-  timeRef.value = newTimeStr;
-}
+const timeRef = ref("");
+setInterval(
+  (): void => {
+    const newTime = new Date();
+    const newTimeStr = newTime.toLocaleTimeString();
+    timeRef.value = newTimeStr;
+  },
+  1000
+);
 
-setInterval(updateTime, 1000);
-
-const PI = ref(Math.PI);
-const radius = ref(1);
+const data = reactive({
+  pi: Math.PI,
+  radius: 1,
+});
+//const PI = ref(Math.PI);
+//const radius = ref(1);
 const area = computed(
   (): number => {
-    return radius.value * radius.value * PI.value;
+    return data.radius * data.radius * data.pi;
   }
 );
 
 setInterval((): void => {
-    radius.value = Math.round(Math.random() * 10);
+    data.radius = Math.round(Math.random() * 10);
   }, 
   2000
 );
@@ -32,5 +35,5 @@ setInterval((): void => {
 <template>
   <p><h1>ちわーっす {{ name }} くん</h1></p><br>
   <p><h2> いま {{ timeRef }} だよ</h2></p>
-  <p><h2>半径 {{ radius }} の円の面積は {{ area }} です。</h2></p>
+  <p><h2>半径 {{ data.radius }} の円の面積は {{ area }} です。<br>(π={{ data.pi }})</h2></p>
 </template>
